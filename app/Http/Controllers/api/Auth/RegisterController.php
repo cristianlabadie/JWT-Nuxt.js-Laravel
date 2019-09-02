dfe44d07979b643d9424fe87a949b69e86a7b656
Cristian <cristian.labadie@gmail.com>
2019-09-02 18:25:42 -0400

    Register form added ⚡️

--- app/Http/Controllers/api/Auth/RegisterController.php
+++ app/Http/Controllers/api/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
       return response()->json([
         'success' => false,
         'errors' => $validator->errors()
-      ]);
+      ], 422);
     }
 
     /**
